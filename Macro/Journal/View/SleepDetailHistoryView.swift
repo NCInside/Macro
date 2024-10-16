6ea083bb9de5c99b6cf54911a1807f6c152d1314
NCInside <nchristian02@ciputra.ac.id>
2024-10-16 14:03:38 +0700

    add history and dynamic journal content

---
+++ Macro/Journal/View/SleepDetailHistoryView.swift
@@ -0,0 +1,53 @@
+//
+//  SleepDetailHistoryView.swift
+//  Macro
+//
+//  Created by Nicholas Christian Irawan on 16/10/24.
+//
+
+import SwiftUI
+
+struct SleepDetailHistoryView: View {
+    
+    var sleep: Sleep
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter
+    }()
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 0) {
+                HStack {
+                    Text(parseSleepDuration(sleep: sleep))
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundStyle(.black)
+                    Text("\(sleep.start, formatter: dateFormatter) - \(sleep.end, formatter: dateFormatter)")
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .foregroundStyle(.gray)
+                }
+                .background(Color(UIColor.systemBackground))
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding(.horizontal)
+            .padding(.vertical, 12)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .background(Color.background)
+        .navigationTitle("Sleep")
+    }
+    
+    private func parseSleepDuration(sleep: Sleep) -> String {
+        let hour: Int = sleep.duration / 3600
+        let minut: Int = (sleep.duration % 3600) / 60
+        
+        return "\(hour)hrs \(minut)min"
+    }
+    
+}
